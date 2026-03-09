66b2905981d8fd4797cb304a6cf6f2331f7c786b
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:30:56 -0600

    Ejercicio 7 agregado

---
+++ Funciones07/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+class program
+{
+    static void MostrarDoble(int numero)
+    {
+        Console.WriteLine("El doble del numero es: " + (numero*2));
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un numero:");
+        int numero;
+        numero = int.Parse(Console.ReadLine());
+        MostrarDoble(numero);
+
+        Console.ReadKey();
+    }
+}
