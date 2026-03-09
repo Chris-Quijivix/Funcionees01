a4a01b429d8f371e20b5b22919d16d741ee3095c
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:41:02 -0600

    Ejercicio 9 agregado

---
+++ Funciones09/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+class program
+{
+    static void SumarMostrar(int a, int b)
+    {
+        Console.WriteLine("La suma total es: " + (a+b));
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingres el primer numero: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo numero: ");
+        int b = int.Parse(Console.ReadLine());
+
+        SumarMostrar(a, b);
+    }
+}
+
