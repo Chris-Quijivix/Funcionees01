b0b6348e3ce74bda9fc472b4a63d85efb101f686
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:21:16 -0600

    Ejercicio 5 agregado

---
+++ Funciones05/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+class program
+{
+    static void MostrarMenu()
+    {
+        Console.WriteLine("1. Opcion1");
+        Console.WriteLine("2. Opcion2");
+        Console.WriteLine("3. Opcion3");
+    }
+
+    static void Main()
+    {
+        MostrarMenu();
+
+        Console.ReadKey();
+    }
+
+}
