ab0f8a16879e2520985342011aff7ed488005118
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:33:23 -0600

    Ejercicio 19 agregado

---
+++ Funciones19/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+class program
+{
+    static void EsMultiplo(int a, int b)
+    {
+        if (a %  b == 0)
+        {
+            Console.WriteLine($" {a} Es multiplo de :" + b);
+        }
+        else
+        {
+            Console.WriteLine($" {a} No es multiplo de : " + b);
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese el primer numero: ");
+        int a = int.Parse( Console.ReadLine() );
+        Console.WriteLine();
+
+        Console.Write("Ingrese el segundo numero: ");
+        int b = int.Parse( Console.ReadLine() );    
+        Console.WriteLine();
+
+        EsMultiplo(a, b);
+
+        Console.ReadKey();
+    }
+}
