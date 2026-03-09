5c4c26d225830b15422d5fa58f0c819331b589b3
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 13:52:20 -0600

    Ejercicio 10 agregado

---
+++ Funciones10/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+class program
+{
+    static void ImprimirRepetido(string texto,int cantidad)
+    {
+        for (int i =0; i < cantidad; i++)
+        {
+            Console.WriteLine(texto);
+        }
+    }
+
+    static void Main ()
+    {
+        Console.Write("Ingrese el texto a mostrar: ");
+        string texto = Console.ReadLine();
+
+        Console.Write("Cuantas veces se repetira el texto?: ");
+        int cantidad = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        ImprimirRepetido(texto, cantidad);
+
+        Console.ReadKey();
+    }
+}
