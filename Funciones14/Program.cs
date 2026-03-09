8c40094b99847dd78aa8b773dea7e111db3af6d5
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:08:14 -0600

    Ejercicio 14 agregado

---
+++ Funciones14/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+class program
+{
+    static double Dividir(double a, double b)
+    {
+        return a / b;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine($"La division de 5/5 es:" + Dividir(5, 5));
+
+        Console.ReadKey();
+    }
+}
