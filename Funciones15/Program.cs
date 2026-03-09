90e8cb5b315b33f2f1ef1da5208ba1857e2f64ba
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:13:16 -0600

    Ejercicio 15 agregado

---
+++ Funciones15/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+class program
+{
+    static void EsPar(int numero)
+    {
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero ingresado es par");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("El numero ingresado no es par");
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un numero:");
+        int numero = int.Parse(Console.ReadLine());
+        EsPar(numero);
+
+        Console.ReadKey();
+    }
+}
