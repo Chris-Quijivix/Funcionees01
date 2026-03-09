a6cb6a645be6d52603892ef73a061b96f2f49778
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:03:21 -0600

    Ejercicio 12 agregado

---
+++ Funciones12/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System;
+class program
+{
+    static int Restar(int a, int b)
+    {
+        return a - b;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine($"La resta de 10-5 es: "+Restar (10,5));
+
+        Console.ReadKey();
+    }
+}
