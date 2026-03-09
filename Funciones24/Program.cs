fc951e135af4e06f668a7ba1791c3d89f0a969c1
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 15:25:02 -0600

    Ejercicio 24 agregado

---
+++ Funciones24/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+class program
+{
+    static int Factorial(int n)
+    {
+        int resultado = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            resultado *= i;
+        }
+
+        return resultado;
+
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("El factorial del numero (5) es: ");
+        Console.WriteLine(Factorial(5));
+
+        Console.ReadKey();
+    }
+}
