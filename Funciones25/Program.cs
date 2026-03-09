2d223a4f56b5f1c3542b963472f50dc90384c8fc
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 15:31:18 -0600

    Ejercicio 25 agregado

---
+++ Funciones25/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+class program
+{
+    static void EsPrimo(int n)
+    {
+        int divisor = 0;
+
+        for (int i = 2; i < n; i++)
+        {
+            if ( n % i  == 0 )
+            {
+                divisor++;
+            }
+        }
+
+        if  ( divisor == 0 )
+        {
+            Console.WriteLine("Es numero primo ");
+        }
+        else
+        {
+            Console.WriteLine("No es mnumero primo");
+        }
+
+
+    }
+
+    static void Main ()
+    {
+        Console.Write("Ingrese un numero: ");
+        int n = int.Parse( Console.ReadLine() );
+        Console.WriteLine();
+        EsPrimo(n);
+
+        Console.ReadKey();
+    }
+
+}
