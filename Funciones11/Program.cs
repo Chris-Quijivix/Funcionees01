f232f967b67859416a17cd9a3019ebb7ce7f25ac
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 13:57:01 -0600

    Ejercicio 11 agregado

---
+++ Funciones11/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+class program
+{
+    static int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine($"La suma de 5+5 es:"+Sumar (5,5));
+
+        Console.ReadKey();
+    }
+}
+
