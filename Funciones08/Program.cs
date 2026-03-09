ece95f6e91f3a366590a43ecce5b69070669298f
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:35:05 -0600

    Ejercicio 8 agregado

---
+++ Funciones08/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+class program
+{
+    static void MostrarCuadrado(int numero)
+    {
+        Console.Write("El cuadrado del numero es: " + (numero * numero));
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un numero: ");
+        int numero;
+        numero = int.Parse(Console.ReadLine());
+        MostrarCuadrado(numero);
+
+        Console.ReadKey();
+    }
+}
