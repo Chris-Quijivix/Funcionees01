66ab428e5bf78f0b520acb9148e3f485d065a110
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:05:34 -0600

    Agregar archivos de proyecto.

---
+++ Funcionees01/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+class program
+{
+    static void saludar()
+    {
+        Console.WriteLine("Hola, bienvenido");
+    }
+
+    static void Main()
+    {
+        saludar();
+
+        Console.ReadKey();
+    }
+}
+
