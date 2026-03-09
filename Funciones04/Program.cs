cedd9d68d65a04a95ca0d181eb2b14252e5d5494
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:18:41 -0600

    Ejercicio 4 agregado

---
+++ Funciones04/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System;
+class program
+{
+    static void Linea()
+    {
+        Console.WriteLine("-----------------------");
+    }
+
+    static void Main()
+    {
+        Linea();
+
+        Console.ReadKey();
+    }
+}
