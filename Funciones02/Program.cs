3b2a055d7cf4c48c439d1d161ced6a7f674f64f7
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:11:26 -0600

    Ejercicio 2 agregado

---
+++ Funciones02/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+class program
+{
+    static void MostrarNombre()
+    {
+        Console.WriteLine("Christopher Quijivix");
+    }
+
+    static void Main()
+    {
+        MostrarNombre();
+
+        Console.ReadKey();
+    }
+
+}
