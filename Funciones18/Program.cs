b69bfa13949136142a0cfed70b75893cb924737d
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:27:18 -0600

    Ejercicio 18 agregado

---
+++ Funciones18/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+class program
+{
+    static int MenorDeTres(int a, int b, int c)
+    {
+        int menor;
+
+        if (a < b && a <  c)
+        {
+            menor = a;
+        }
+        else if(b < c && b < a)
+        {
+            menor = b;
+        }
+        else
+        {
+            menor = c;
+        }
+
+        return menor;
+
+    }
+
+    static void Main()
+    {
+        Console.WriteLine($"El numero menor entre 5,6,1 es:" + MenorDeTres(5, 6, 1));
+
+        Console.ReadKey();
+    }
+}
