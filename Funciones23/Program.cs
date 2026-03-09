04c41f4d392049fb842cbfae2f56e5ece74edf40
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 15:22:17 -0600

    Ejercicio 23 agregado

---
+++ Funciones23/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+class program
+{
+    static int SumarDel1AlN(int n)
+    {
+        int suma = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            suma += i;
+        }
+
+        return suma;
+        
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(SumarDel1AlN(5));
+    }
+}
