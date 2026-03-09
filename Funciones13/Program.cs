9fde62834a4ef9d22df05986d1ca83b331b46822
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:06:17 -0600

    Ejercicio 13 agregado

---
+++ Funciones13/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+class program
+{
+    static int Multiplicar(int a, int b)
+    {
+        return a * b;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine($"La multiplicacion de 5*5 es: " + Multiplicar(5,5));
+
+        Console.ReadKey();
+    }
+}
