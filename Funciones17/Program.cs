ce84aaa0a9c5dab3cdad17c820765733aaf1e8db
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:20:50 -0600

    Ejercicio 17 agregado

---
+++ Funciones17/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+class program
+{
+    static int Mayor(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine($"El numero mayor entre 5 y 10 es: "+ Mayor(5, 10));
+
+        Console.ReadKey();
+    }
+}
