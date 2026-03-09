9b09125f67f5199a22c628fa935076a843b2b6f9
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:17:42 -0600

    Ejercicio 16 agregado

---
+++ Funciones16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class program
+{
+    static void EsPositivo(int numero)
+    {
+        if (numero > 0)
+        {
+            Console.WriteLine("El numero es positivo");
+        }
+        else if (numero < 0) 
+        {
+            Console.WriteLine("El numero no es positivo");
+        }
+        else
+        {
+            Console.WriteLine("El numero es cero");
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un numero: ");
+        int numero = int.Parse(Console.ReadLine());
+        EsPositivo(numero);
+
+        Console.ReadKey();
+    }
+}
