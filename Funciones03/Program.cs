bc30ab5d62fb35478424490ce904c30d62ba369a
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:16:42 -0600

    Ejercicio 3 agregado

---
+++ Funciones03/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+class program
+{
+    static void MostrarMensaje()
+    {
+        Console.WriteLine("Programacion en C#");
+    }
+
+    static void Main()
+    {
+        MostrarMensaje();
+
+        Console.ReadKey();
+    }
+    
+}
