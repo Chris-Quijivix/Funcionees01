a2439496676f27b6f8327c2eeb54ad68aea07339
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 14:38:24 -0600

    Ejercicio 20 agregado

---
+++ Funciones20/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+class program
+{
+    static int ContarCaracteres(string texto)
+    {
+        Console.WriteLine($"El texto tiene: " );
+        return texto.Length;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(ContarCaracteres("Programacion"));
+
+        Console.ReadKey();
+    }
+}
