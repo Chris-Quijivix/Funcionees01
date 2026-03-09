c621ab290bc9cef3e0b60baf376f2f3f1a63996e
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 12:25:46 -0600

    Ejercicio 6 agregado

---
+++ Funciones06/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+class program
+{
+    static void Saludar(string nombre)
+    {
+        Console.WriteLine("Hola " + nombre);
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese un nombre:");
+        string nombre;
+        nombre = Console.ReadLine();
+        Saludar(nombre);
+    }
+}
