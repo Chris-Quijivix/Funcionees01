91e9294fcc1e8a179fc20a5c3f2216ddcd0a54c8
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 15:18:51 -0600

    Ejercicio 22 agregado

---
+++ Funciones22/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+class program
+{
+    static string Repetir(String texto, int veces)
+    {
+        string mostrar = "";
+
+        for (int i = 0; i < veces; i++)
+        {
+            mostrar += texto;
+        }
+
+        return mostrar;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Repetir("Programacion ", 3));
+
+        Console.ReadKey();
+    }
+}
