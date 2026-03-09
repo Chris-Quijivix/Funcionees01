96426c865e82f164d987e25e11739311fda7a9d1
Christopher Quijivix <chrisquijivix12@gmail.com>
2026-03-09 15:14:59 -0600

    Ejercicio 21 agregado

---
+++ Funciones21/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+class program
+{
+    static int ContarVocales(string texto)
+    {
+        int contador = 0;
+
+        foreach (char c in texto.ToLower())
+        {
+            if ("aeiou".Contains(c))
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("El texto tiene");
+        Console.WriteLine(ContarVocales("Programacion"));
+        Console.WriteLine("Vocales");
+
+        Console.ReadKey();
+    }
+}
